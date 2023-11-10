a2b572ebed3166fbe1f155c309bf4b2484857ed6
Anton Terentev <gamervii@icloud.com>
2023-11-10 10:35:24 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Modrinth.Api
+=======
+
---
+++ tests/Modrinth.Tests/ModrinthApiTests.cs
@@ -0,0 +1,18 @@
+namespace Modrinth.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+
+        
+
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
