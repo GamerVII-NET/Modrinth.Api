18ab7806ec3e73daa90fb678c9a546596a8e9abf
GamerVII-NET <111225722+GamerVII-NET@users.noreply.github.com>
2023-11-11 19:49:31 +0300

    merge branches dev and main  (#1)
    * Implemented the following testing methods: End for concluding the test scenario, FindModById for finding a mod by Id, FindProjectById for finding a project by Id, FindProjectBySlug for finding a project by Slug, GetModsList for retrieving a list of mods, GetModVersions for getting mod versions, InitApi for initializing the API before testing, and InitProjectsList for initializing the list of projects, added the possibility to download mods

---
+++ src/Modrinth.Api/Core/Projects/Mods.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Modrinth.Api.Core.Repository;
+using Modrinth.Api.Core.System;
+using Modrinth.Api.Models.Dto;
+using Modrinth.Api.Models.Projects;
+
+namespace Modrinth.Api.Core.Projects
+{
+    public class Mods : Projects, IProjectRepository
+    {
+        private readonly ModrinthApi _api;
+        private readonly FileLoader _fileLoader;
+
+        public Mods(ModrinthApi api, HttpClientFactory httpClientFactory) : base(api, httpClientFactory)
+        {
+            _api = api;
+            _fileLoader = new FileLoader();
+        }
+
+        public new async Task<TProject> FindAsync<TProject>(string identifier, CancellationToken token)
+        {
+            var project = await base.FindAsync<TProject>(identifier, token);
+
+            if (project is Project modProject)
+                modProject.Api = _api;
+
+            return project;
+        }
+
+        public Task DownloadAsync(string path, Version version, bool loadDependencies, CancellationToken token)
+        {
+            return DownloadAsync(new DirectoryInfo(path), version, loadDependencies, token);
+        }
+
+        public async Task DownloadAsync(DirectoryInfo directory, Version version, bool loadDependencies, CancellationToken token)
+        {
+            var filesUrls = version.Files.Select(c => c.Url).ToList();
+
+            if (loadDependencies)
+            {
+                var dependenciesVersions = await version.GetRecursiveDependenciesUrlsAsync(token);
+
+                var dependencyFiles = dependenciesVersions.SelectMany(c => c.Files).Select(c => c.Url);
+
+                filesUrls.AddRange(dependencyFiles);
+            }
+
+            await _fileLoader.DownloadFilesAsync(filesUrls.ToArray(), directory.FullName, token);
+
+        }
+
+        public async Task<Version?> GetLastVersionAsync(string identifier, CancellationToken token)
+        {
+            var versions = await GetProjectVersions(identifier, token);
+
+            return versions.OrderByDescending(c => c.DatePublished).FirstOrDefault();
+        }
+    }
+}
---
+++ src/Modrinth.Api/Core/Projects/Versions.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Modrinth.Api.Core.Endpoints;
+using Modrinth.Api.Core.System;
+using Modrinth.Api.Models.Dto;
+
+namespace Modrinth.Api.Core.Projects
+{
+    public class Versions
+    {
+        private readonly ModrinthApi _api;
+        private readonly HttpClientFactory _httpClientFactory;
+
+        public Versions(ModrinthApi modrinthApi, HttpClientFactory httpClientFactory)
+        {
+            _api = modrinthApi;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IEnumerable<Version>> GetDependenciesAsync(Version version, CancellationToken token)
+        {
+            var dependencyVersions = new List<Version>();
+
+            foreach (var dependency in version.Dependencies)
+            {
+                Version? dependencyVersion = null;
+                if (!string.IsNullOrEmpty(dependency.VersionId))
+                {
+                    dependencyVersion = await GetVersionById(dependency.VersionId, token);
+                }
+
+                if (!string.IsNullOrEmpty(dependency.ProjectId))
+                {
+                    dependencyVersion = await _api.Mods.GetLastVersionAsync(dependency.ProjectId, token);
+                }
+
+                if (dependencyVersion != null)
+                    dependencyVersions.Add(dependencyVersion);
+            }
+
+            return dependencyVersions;
+        }
+
+        private async Task<Version?> GetVersionById(string identifier, CancellationToken token)
+        {
+            var endPoint = ModrinthEndpoints.Version.Replace("{id}", identifier);
+
+            var response = await _httpClientFactory.HttpClient.GetAsync(endPoint, token);
+
+            RequestHelper.UpdateApiRequestInfo(_api, response);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return default;
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            return JsonSerializer.Deserialize<Version>(content) ?? null;
+        }
+    }
+}
